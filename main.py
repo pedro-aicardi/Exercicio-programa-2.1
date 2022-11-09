c1b3158202f4771cbc70a17b1a547747544fce06
vitorpadovani <vipadovani28@gmail.com>
2022-11-09 14:44:49 -0300

    inicio programa principal

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n')
+                                                                                
+nome = input('Qual seu nome? ')                                                             
+                                                                                
+print(f'\nOk {nome}, você tem direito a pular 3 vezes e 2 ajudas!')
+print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n') #COR       
+                                                                                
+input('Aperte ENTER para continuar...')                                                  
+                                                                                
+print('\nO jogo já vai começar! Lá vem a primeira questão!')
+print('\nVamos começar com questões do nível FACIL!')                             
+                                                                                
+input('Aperte ENTER para continuar...')
+
+
+print('\n\n' + '-'*40)
