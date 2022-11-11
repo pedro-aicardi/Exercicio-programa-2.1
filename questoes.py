44cb0c85da0c89538de31ec7bac8dd6f70e144e6
vitorpadovani <vipadovani28@gmail.com>
2022-11-11 09:41:20 -0300

    criando novas questoes

---
+++ questoes.py
@@ -0,0 +1,32 @@
+[
+  {
+    'titulo': 'Qual um sinonimo para mexerica',
+    'nivel': 'facil',
+    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
+    'correta': 'B'
+  },
+  {
+    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
+    'nivel': 'facil',
+    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
+    'correta': 'A'
+  },
+  {
+    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'correta': 'B'
+  },
+  {
+    'titulo': 'Quantos filmes tem a saga "Star Wars',
+    'nivel': 'medio',
+    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
+    'correta': 'D' #necessita de dica (não contar rogue one)#
+  },
+  {
+    'titulo': 'Qual a capital da Grécia',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
+    'correta': 'A'
+  },
+]
