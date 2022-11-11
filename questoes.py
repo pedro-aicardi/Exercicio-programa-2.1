036cd0e8f9dc373e8a517826e28a786efcf6264c
vitorpadovani <vipadovani28@gmail.com>
2022-11-11 15:47:21 -0300

    adicionando questoes

--- questoes.py
+++ questoes.py
@@ -1,12 +1,12 @@
 lista_questoes = [
   {#1
-    'titulo': 'Qual um sinonimo para mexerica',
+    'titulo': 'Qual um sinonimo para mexerica?',
     'nivel': 'facil',
     'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
     'correta': 'B'
   },
   {#2
-    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
+    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."?',
     'nivel': 'facil',
     'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
     'correta': 'A'
@@ -18,55 +18,55 @@ lista_questoes = [
     'correta': 'B'
   },
   {#4
-    'titulo': 'Quantos filmes tem a saga "Star Wars',
+    'titulo': 'Quantos filmes tem a saga "Star Wars"?',
     'nivel': 'medio',
     'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
     'correta': 'D' #necessita de dica (não contar rogue one)#
   },
   {#5
-    'titulo': 'Qual a capital da Grécia',
+    'titulo': 'Qual a capital da Grécia?',
     'nivel': 'medio',
     'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
     'correta': 'A'
   },
   {#6
-    'titulo': 'Qual um sinonimo para mexerica',
-    'nivel': 'facil',
-    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
-    'correta': 'B'
+    'titulo': 'Onde o sol nasce e se põe, respectivamente?',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Oeste/Leste', 'B': 'Norte/Sul', 'C': 'Sul/Norte', 'D': 'Leste/Oeste'},
+    'correta': 'D'
   },
   {#7
-    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
-    'nivel': 'facil',
-    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
-    'correta': 'A'
+    'titulo': 'Qual o nome do filho do Neymar?',
+    'nivel': 'dificil',
+    'opcoes': {'A': 'Matheus Henrique', 'B': 'Antonio', 'C': 'Davi Lucca', 'D': 'José Pedro'},
+    'correta': 'C'
   },
   {#8
-    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
-    'nivel': 'medio',
-    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'titulo': 'Qual novela teve a maior audiencia na Tv Globo?',
+    'nivel': 'dificil',
+    'opcoes': {'A': 'Salve Jorge (2013)', 'B': 'Roque Santeiro (1985)', 'C': 'O Salvador da Pátria (1989)', 'D': 'A Força do Querer (2017)'},
     'correta': 'B'
   },
   {#9
-    'titulo': 'Quantos filmes tem a saga "Star Wars',
-    'nivel': 'medio',
-    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
-    'correta': 'D' #necessita de dica (não contar rogue one)#
+    'titulo': 'Qual a cor resultante da adição da cor vermelha com a azul?',
+    'nivel': 'facil',
+    'opcoes': {'A': 'Roxo/Violeta', 'B': 'Azul', 'C': 'Vinho', 'D': 'Ciano/Azul claro'},
+    'correta': 'A' 
   },
   {#10
-    'titulo': 'Qual a capital da Grécia',
+    'titulo': 'Em que ano a seleção brasileira foi pentacampeão do mundo?',
     'nivel': 'medio',
-    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
-    'correta': 'A'
+    'opcoes': {'A': '1994', 'B': '1998', 'C': '2002', 'D': '2006'},
+    'correta': 'C'
   },
   {#11
-    'titulo': 'Qual um sinonimo para mexerica',
+    'titulo': 'Qual o maior animal do mundo?',
     'nivel': 'facil',
-    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
+    'opcoes': {'A': 'Girafa', 'B': 'Baleia Azul', 'C': 'Hipopotámo', 'D': 'Urso'},
     'correta': 'B'
   },
   {#12
-    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
+    'titulo': '',
     'nivel': 'facil',
     'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
     'correta': 'A'
