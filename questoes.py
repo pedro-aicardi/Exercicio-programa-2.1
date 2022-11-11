ea28ba9f67f033767e81c5fdd8d75eee51bef519
vitorpadovani <vipadovani28@gmail.com>
2022-11-11 12:59:06 -0300

    adicionando questoes

--- questoes.py
+++ questoes.py
@@ -1,32 +1,124 @@
-[
-  {
+lista_questoes = [
+  {#1
     'titulo': 'Qual um sinonimo para mexerica',
     'nivel': 'facil',
     'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
     'correta': 'B'
   },
-  {
+  {#2
     'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
     'nivel': 'facil',
     'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
     'correta': 'A'
   },
-  {
+  {#3
     'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
     'nivel': 'medio',
     'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
     'correta': 'B'
   },
-  {
+  {#4
     'titulo': 'Quantos filmes tem a saga "Star Wars',
     'nivel': 'medio',
     'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
     'correta': 'D' #necessita de dica (não contar rogue one)#
   },
-  {
+  {#5
+    'titulo': 'Qual a capital da Grécia',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
+    'correta': 'A'
+  },
+  {#6
+    'titulo': 'Qual um sinonimo para mexerica',
+    'nivel': 'facil',
+    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
+    'correta': 'B'
+  },
+  {#7
+    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
+    'nivel': 'facil',
+    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
+    'correta': 'A'
+  },
+  {#8
+    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'correta': 'B'
+  },
+  {#9
+    'titulo': 'Quantos filmes tem a saga "Star Wars',
+    'nivel': 'medio',
+    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
+    'correta': 'D' #necessita de dica (não contar rogue one)#
+  },
+  {#10
+    'titulo': 'Qual a capital da Grécia',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
+    'correta': 'A'
+  },
+  {#11
+    'titulo': 'Qual um sinonimo para mexerica',
+    'nivel': 'facil',
+    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
+    'correta': 'B'
+  },
+  {#12
+    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
+    'nivel': 'facil',
+    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
+    'correta': 'A'
+  },
+  {#13
+    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'correta': 'B'
+  },
+  {#14
+    'titulo': 'Quantos filmes tem a saga "Star Wars',
+    'nivel': 'medio',
+    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
+    'correta': 'D' #necessita de dica (não contar rogue one)#
+  },
+  {#15
+    'titulo': 'Qual a capital da Grécia',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
+    'correta': 'A'
+  },
+  {#16
+    'titulo': 'Qual um sinonimo para mexerica',
+    'nivel': 'facil',
+    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
+    'correta': 'B'
+  },
+  {#17
+    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
+    'nivel': 'facil',
+    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
+    'correta': 'A'
+  },
+  {#18
+    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'correta': 'B'
+  },
+  {#19
+    'titulo': 'Quantos filmes tem a saga "Star Wars',
+    'nivel': 'medio',
+    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
+    'correta': 'D' #necessita de dica (não contar rogue one)#
+  },
+  {#20
     'titulo': 'Qual a capital da Grécia',
     'nivel': 'medio',
     'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
     'correta': 'A'
   },
 ]
+
+print(lista_questoes)
