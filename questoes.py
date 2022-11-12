38122a817d72052507c814ab8d6e4762433a56ea
vitorpadovani <vipadovani28@gmail.com>
2022-11-12 00:15:19 -0300

    20 questoes

--- questoes.py
+++ questoes.py
@@ -66,58 +66,58 @@ lista_questoes = [
     'correta': 'B'
   },
   {#12
-    'titulo': '',
+    'titulo': 'Quantas pernas a maioria das aranhas tem? ',
     'nivel': 'facil',
-    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
-    'correta': 'A'
+    'opcoes': {'A': '5', 'B': '6', 'C': '7', 'D': '8'},
+    'correta': 'D'
   },
   {#13
-    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
+    'titulo': 'Qual o instrumento mais conhecido no mundo?',
     'nivel': 'medio',
-    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'opcoes': {'A': 'Bateria', 'B': 'Guitarra', 'C': 'Piano', 'D': 'Flauta'},
     'correta': 'B'
   },
   {#14
-    'titulo': 'Quantos filmes tem a saga "Star Wars',
-    'nivel': 'medio',
-    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
-    'correta': 'D' #necessita de dica (não contar rogue one)#
-  },
-  {#15
-    'titulo': 'Qual a capital da Grécia',
-    'nivel': 'medio',
-    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
+    'titulo': 'Quem é o artista mais ouvido no Spotify?',
+    'nivel': 'dificil',
+    'opcoes': {'A': 'Drake', 'B': 'Justin Bieber', 'C': 'Ariana Grande', 'D': 'Billie Eilish'},
     'correta': 'A'
   },
+  {#15
+    'titulo': 'Quando "Romeu e Julieta", por William Shakespeare, foi publicado?',
+    'nivel': 'dificil',
+    'opcoes': {'A': '1900', 'B': '2019', 'C': '1597', 'D': '1658'},
+    'correta': 'C'
+  },
   {#16
-    'titulo': 'Qual um sinonimo para mexerica',
-    'nivel': 'facil',
-    'opcoes': {'A': 'macaxeira', 'B': 'bergamota', 'C': 'aipim', 'D': 'acerola'},
+    'titulo': 'Complete o seguinte ditado popular brasileiro: "Para bom entendedor,_________" ',
+    'nivel': 'medio',
+    'opcoes': {'A': 'uma imagem serve', 'B': 'meia palavra basta', 'C': 'tudo é mais fácil', 'D': 'meia palavra é o suficiente'},
     'correta': 'B'
   },
   {#17
-    'titulo': 'Qual a continuação do trecho da música "Garota de Ipanema", de Antônio Carlos Jobim e Vinícius de Moraes: "Olha que coisa mais linda Mais cheia de graça É ela, menina..."',
-    'nivel': 'facil',
-    'opcoes': {'A': '...que vem e que passa', 'B': '...que fica na raiva', 'C': '...que fica na praça', 'D': '...que bebe cachaça'},
-    'correta': 'A'
+    'titulo': 'Quem foi o(a) vencedor(a) do primeiro MasterChef Brasil, em 2014?',
+    'nivel': 'dificil',
+    'opcoes': {'A': 'Leo Young', 'B': 'Mohamad Hindi Neto', 'C': 'Elisa Fernandes', 'D': 'Henrique Fogaça'},
+    'correta': 'C'
   },
   {#18
-    'titulo': 'Quem é o apresentador mais velho da televisão brasileira?',
-    'nivel': 'medio',
-    'opcoes': {'A': 'Fausto Silva', 'B': 'Silvio Santos', 'C': 'Téo José', 'D': 'Raul Gil'},
+    'titulo': 'Quantos vertices tem um triângulo?',
+    'nivel': 'facil',
+    'opcoes': {'A': '1', 'B': '2', 'C': '3', 'D': '4'},
     'correta': 'B'
   },
   {#19
-    'titulo': 'Quantos filmes tem a saga "Star Wars',
-    'nivel': 'medio',
-    'opcoes': {'A': '6', 'B': '7', 'C': '8', 'D': '9'},
+    'titulo': 'Qual o filme com maior bilheteria na história?',
+    'nivel': 'dificil',
+    'opcoes': {'A': '"Avatar"', 'B': '"Vingadores: Ultimato"', 'C': 'Titanic', 'D': '"Star Wars: O Despertar da Força"'},
     'correta': 'D' #necessita de dica (não contar rogue one)#
   },
   {#20
-    'titulo': 'Qual a capital da Grécia',
-    'nivel': 'medio',
-    'opcoes': {'A': 'Iráklio', 'B': 'Pireu', 'C': 'Olimpia', 'D': 'Atenas'},
-    'correta': 'A'
+    'titulo': 'Quem é o dono da tesla',
+    'nivel': 'facil',
+    'opcoes': {'A': 'Bill Gates', 'B': 'Elon Musk', 'C': 'Branislav Ivanović', 'D': 'Stuart Phillips'},
+    'correta': 'B'
   },
 ]
 
